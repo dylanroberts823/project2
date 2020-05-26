206c44df71207558e209675e1e7c97d4965b0eb7
dylanroberts823 <jguidote@hotmail.com>
2020-05-26 07:00:21 -0400

    Good heading, no submit action

--- application.py
+++ application.py
@@ -7,25 +7,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-username = "";
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-#Check if they have a username
-    if username == "":
-        #Since they don't have a username, have them sign up
-        return redirect("/sign_up")
-    else:
-        #Since they've chosen a username, forward to main page with Username
-        return redirect("/chatroom")
+    return render_template("index.html")
 
-@app.route("/sign_up", methods=["GET", "POST"])
+@app.route("/sign_up")
 def sign_up():
-    if request.method == "GET":
-        return render_template("sign_up.html")
-
-    elif request.method == "POST":
-        return redirect("/chatroom")
+    return render_template("sign_up.html")
 
 @app.route("/chatroom")
 def chatroom():
