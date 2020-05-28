11461d42368581940fe32414b07d2dc09819e6f6
dylanroberts823 <jguidote@hotmail.com>
2020-05-28 10:16:49 -0400

    Put back script

--- static/index.js
+++ static/index.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
+    // By default, submit button is disabled
+    document.querySelector('#submit').disabled = true;
+
+    // Enable button only if there is text in the input field
+    document.querySelector('#task').onkeyup = () => {
+        if (document.querySelector('#task').value.length > 0)
+            document.querySelector('#submit').disabled = false;
+        else
+            document.querySelector('#submit').disabled = true;
+    };
+
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
