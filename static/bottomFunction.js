dac251ac0c82a9c3209a486d3ec84445c59b21e1
dylanroberts823 <jguidote@hotmail.com>
2020-06-04 14:38:10 -0400

    Enabled button to hide on loading the page

--- static/bottomFunction.js
+++ static/bottomFunction.js
@@ -3,10 +3,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   mybutton = document.getElementById("myBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
+  //On loading page, if window is smaller than document, hide button
+  if (window.innerHeight + 100 >= document.body.offsetHeight){
+    mybutton.style.display = "none";
+    mybutton.className = "fixed-bottom mx-auto btn btn-info mb-2";
+    mybutton.innerHTML = "Bottom"
+  };
+
+  // When the user scrolls down 50px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
 
   function scrollFunction() {
+    //If we're at the bottom of the page, hide button
     if (window.innerHeight + window.scrollY + 100 >= document.body.offsetHeight) {
       mybutton.style.display = "none";
       mybutton.className = "fixed-bottom mx-auto btn btn-info mb-2";
